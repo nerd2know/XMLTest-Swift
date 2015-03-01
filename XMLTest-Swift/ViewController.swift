606b66da3c9143b931bf19b4ffadad106237c2ed
Brian Thompson <brian@nerd2know.net>
2015-02-28 18:58:36 -0600

    Initial Commit

---
+++ XMLTest-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMLTest-Swift
+//
+//  Created by Wile E on 2/28/15.
+//  Copyright (c) 2015 Example.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
